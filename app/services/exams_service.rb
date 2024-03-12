183bb3712ff6f4a262e9ba400746868925f35ce1
Mateus Cavedini Pereira <mateuscavedini@gmail.com>
2024-03-12 03:44:22 -0300

    Move e refatora método de TestsService para ExamsService

--- app/services/exams_service.rb
+++ app/services/exams_service.rb
@@ -5,6 +5,18 @@ class ExamsService
     @repository = repository.new conn: conn
   end
 
+  def fetch_all(close_conn: true)
+    @repository.all(close_conn: close_conn).map do |row|
+      {
+        token: row['token'],
+        date: row['date'],
+        patient: JSON.parse(row['patient']),
+        doctor: JSON.parse(row['doctor']),
+        tests: JSON.parse(row['tests'])
+      }
+    end.to_json
+  end
+
   def batch_insert(batch:, close_conn: true)
     values = batch.map(&:attr_values)
     @repository.batch_insert batch: values, close_conn: close_conn
--- app/services/tests_service.rb
+++ app/services/tests_service.rb
@@ -5,10 +5,6 @@ class TestsService
     @repository = repository.new conn: conn
   end
 
-  def fetch_all(close_conn: true)
-    @repository.all(close_conn: close_conn).to_a.to_json
-  end
-
   def batch_insert(batch:, close_conn: true)
     values = batch.map { |obj| obj.attr_values[1..] }
     @repository.batch_insert batch: values, close_conn: close_conn
