d16d372fb6437c594e0c0bfecc8cf04e18d5cf3c
Mateus Cavedini Pereira <mateuscavedini@gmail.com>
2024-03-12 03:39:59 -0300

    Refatora testes de requests

--- spec/requests/server_spec.rb
+++ spec/requests/server_spec.rb
@@ -1,4 +1,12 @@
 require 'spec_helper'
+require './app/services/patients_service'
+require './app/services/doctors_service'
+require './app/services/exams_service'
+require './app/services/tests_service'
+require './app/models/patient'
+require './app/models/doctor'
+require './app/models/exam'
+require './app/models/test'
 
 describe 'Rebase Labs API' do
   context 'GET /' do
@@ -10,17 +18,58 @@ describe 'Rebase Labs API' do
     end
   end
 
-  context 'GET /tests' do
-    it 'deve retornar dados do csv' do
-      get '/tests'
+  context 'GET /exams' do
+    it 'deve retornar todos os exames registrados' do
+      patients_service = PatientsService.new conn: @conn
+      doctors_service = DoctorsService.new conn: @conn
+      exams_service = ExamsService.new conn: @conn
+      tests_service = TestsService.new conn: @conn
+
+      pedro = Patient.new cpf: '139.363.670-51', name: 'Pedro Godoi Guedes',
+                          email: 'pedro@email.com', birthday: '1990-10-25',
+                          address: 'Rua das Palmeiras, 145', city: 'Rio de Janeiro',
+                          state: 'RJ'
+      daniela = Patient.new cpf: '763.514.890-75', name: 'Daniela Larissa da Silva',
+                            email: 'daniela@email.com', birthday: '2005-04-15',
+                            address: 'Rua Dalila da Costa, 43', city: 'Sumaré',
+                            state: 'SP'
+      patients_service.batch_insert batch: [pedro, daniela], close_conn: false
+
+      doctor_ana = Doctor.new crm: 'B000A7CDX4', crm_state: 'SP',
+                              name: 'Ana da Silva', email: 'ana@email.com'
+      doctor_joao = Doctor.new crm: 'B0000DHDOF', crm_state: 'MA',
+                               name: 'João Lima', email: 'joao@email.com'
+      doctors_service.batch_insert batch: [doctor_ana, doctor_joao], close_conn: false
+
+      pedro_exam = Exam.new token: 'T9O6AI', date: '2021-08-05', patient_cpf: pedro.cpf,
+                            doctor_crm: doctor_ana.crm, doctor_crm_state: doctor_ana.crm_state
+      daniela_exam = Exam.new token: '0W9I67', date: '2021-11-21', patient_cpf: daniela.cpf,
+                              doctor_crm: doctor_joao.crm, doctor_crm_state: doctor_joao.crm_state
+      exams_service.batch_insert batch: [pedro_exam, daniela_exam], close_conn: false
+
+      pedro_hdl_test = Test.new type: 'hdl', limits: '19-75',
+                                result: '13', exam_token: pedro_exam.token
+      daniela_ldl_test = Test.new type: 'ldl', limits: '19-75',
+                                  result: '20', exam_token: daniela_exam.token
+      tests_service.batch_insert batch: [pedro_hdl_test, daniela_ldl_test], close_conn: false
+
+      get '/exams'
 
       json_body = JSON.parse last_response.body
 
       expect(last_response.status).to eq 200
       expect(last_response.content_type).to eq 'application/json'
-      expect(json_body[0]['nome_paciente']).to eq 'Emilly Batista Neto'
-      expect(json_body[0]['nome_medico']).to eq 'Maria Luiza Pires'
-      expect(json_body[0]['token_resultado_exame']).to eq 'IQCZ17'
+      expect(json_body.size).to eq 2
+      expect(json_body[0]['token']).to eq '0W9I67'
+      expect(json_body[0]['patient']['cpf']).to eq '763.514.890-75'
+      expect(json_body[0]['doctor']['crm']).to eq 'B0000DHDOF'
+      expect(json_body[0]['doctor']['crm_state']).to eq 'MA'
+      expect(json_body[0]['tests'][0]['type']).to eq 'ldl'
+      expect(json_body[1]['token']).to eq 'T9O6AI'
+      expect(json_body[1]['patient']['cpf']).to eq '139.363.670-51'
+      expect(json_body[1]['doctor']['crm']).to eq 'B000A7CDX4'
+      expect(json_body[1]['doctor']['crm_state']).to eq 'SP'
+      expect(json_body[1]['tests'][0]['type']).to eq 'hdl'
     end
   end
 end
