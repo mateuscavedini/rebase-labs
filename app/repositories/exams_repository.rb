c92761bcc596bbf07710eaaf035ffbcb4102c631
Mateus Cavedini Pereira <mateuscavedini@gmail.com>
2024-03-13 16:54:49 -0300

    Refatora query de busca de exames e adiciona teste para #all de ExamsRepository

--- app/repositories/exams_repository.rb
+++ app/repositories/exams_repository.rb
@@ -7,23 +7,22 @@ class ExamsRepository < BaseRepository
     ON CONFLICT (token) DO NOTHING;
   SQL
 
+  JOINED_SELECT_SQL = <<-SQL.freeze
+    SELECT e.token, e.date, json_build_object('name', p.name, 'cpf', p.cpf) AS patient,
+           json_build_object('name', d.name, 'crm', d.crm, 'crm_state', d.crm_state) AS doctor
+    FROM exams e
+    JOIN patients p ON e.patient_cpf = p.cpf
+    JOIN doctors d ON e.doctor_crm = d.crm AND e.doctor_crm_state = d.crm_state
+    GROUP BY e.token, e.date, p.name, p.cpf, d.name, d.crm, d.crm_state
+    ORDER BY e.date DESC;
+  SQL
+
   def initialize(conn: nil)
     super(class_name: self.class, conn: conn)
   end
 
   def all(close_conn: true)
-    joined_select_sql = <<-SQL
-      SELECT e.token, e.date, json_build_object('name', p.name, 'cpf', p.cpf) AS patient,
-             json_build_object('name', d.name, 'crm', d.crm, 'crm_state', d.crm_state) AS doctor,
-             json_agg(json_build_object('type', t.type, 'limits', t.limits, 'result', t.result)) AS tests
-      FROM exams e
-      JOIN patients p ON e.patient_cpf = p.cpf
-      JOIN doctors d ON e.doctor_crm = d.crm AND e.doctor_crm_state = d.crm_state
-      JOIN tests t ON e.token = t.exam_token
-      GROUP BY e.token, e.date, p.name, p.cpf, d.name, d.crm, d.crm_state;
-    SQL
-
-    result = @conn.exec joined_select_sql
+    result = @conn.exec JOINED_SELECT_SQL
 
     @conn.close if close_conn
 
--- spec/repositories/exams_repository_spec.rb
+++ spec/repositories/exams_repository_spec.rb
@@ -161,4 +161,70 @@ describe ExamsRepository do
       expect(result.values).to be_empty
     end
   end
+
+  context '#all' do
+    it 'deve retornar todos os exames do banco em order desc de data' do
+      exams_repository = ExamsRepository.new conn: @conn
+      patients_repository = PatientsRepository.new conn: @conn
+      doctors_repository = DoctorsRepository.new conn: @conn
+
+      patient_pedro = Patient.new cpf: '139.363.670-51', name: 'Pedro Godoi Guedes',
+                                  email: 'pedro@email.com', birthday: '1990-10-25',
+                                  address: 'Rua das Palmeiras, 145', city: 'Rio de Janeiro',
+                                  state: 'RJ'
+      patient_daniela = Patient.new cpf: '763.514.890-75', name: 'Daniela Larissa da Silva',
+                                    email: 'daniela@email.com', birthday: '2005-04-15',
+                                    address: 'Rua Dalila da Costa, 43', city: 'Sumaré',
+                                    state: 'SP'
+      patient_bruno = Patient.new cpf: '499.455.830-26', name: 'Bruno Ferreira',
+                                  email: 'bruno@email.com', birthday: '1870-09-18',
+                                  address: 'Rua Doutor Lucas Dias, 5', city: 'Belém',
+                                  state: 'PA'
+      patients_repository.batch_insert batch: [patient_pedro.attr_values, patient_daniela.attr_values,
+                                               patient_bruno.attr_values],
+                                       close_conn: false
+
+      doctor_ana = Doctor.new crm: 'B000A7CDX4', crm_state: 'SP',
+                              name: 'Ana da Silva', email: 'ana@email.com'
+      doctor_luis = Doctor.new crm: 'B000AR99QO', crm_state: 'SC',
+                               name: 'Luis Lopes', email: 'luis@email.com'
+      doctor_joao = Doctor.new crm: 'B0000DHDOF', crm_state: 'MA',
+                               name: 'João Lima', email: 'joao@email.com'
+      doctors_repository.batch_insert batch: [doctor_ana.attr_values, doctor_luis.attr_values,
+                                              doctor_joao.attr_values],
+                                      close_conn: false
+
+      pedro_exam = Exam.new token: 'T9O6AI', date: '2021-08-05', patient_cpf: patient_pedro.cpf,
+                            doctor_crm: doctor_ana.crm, doctor_crm_state: doctor_ana.crm_state
+      daniela_exam = Exam.new token: '0W9I67', date: '2021-11-21', patient_cpf: patient_daniela.cpf,
+                              doctor_crm: doctor_luis.crm, doctor_crm_state: doctor_luis.crm_state
+      bruno_exam = Exam.new token: 'IQCZ17', date: '2021-07-09', patient_cpf: patient_bruno.cpf,
+                            doctor_crm: doctor_joao.crm, doctor_crm_state: doctor_joao.crm_state
+      exams_repository.batch_insert batch: [pedro_exam.attr_values, daniela_exam.attr_values,
+                                            bruno_exam.attr_values],
+                                    close_conn: false
+
+      result = exams_repository.all close_conn: false
+
+      expect(result.num_tuples).to eq 3
+      expect(result.values[0][0]).to eq '0W9I67'
+      expect(result.values[0][2]).to eq '{"name" : "Daniela Larissa da Silva", "cpf" : "763.514.890-75"}'
+      expect(result.values[0][3]).to eq '{"name" : "Luis Lopes", "crm" : "B000AR99QO", "crm_state" : "SC"}'
+      expect(result.values[1][0]).to eq 'T9O6AI'
+      expect(result.values[1][2]).to eq '{"name" : "Pedro Godoi Guedes", "cpf" : "139.363.670-51"}'
+      expect(result.values[1][3]).to eq '{"name" : "Ana da Silva", "crm" : "B000A7CDX4", "crm_state" : "SP"}'
+      expect(result.values[2][0]).to eq 'IQCZ17'
+      expect(result.values[2][2]).to eq '{"name" : "Bruno Ferreira", "cpf" : "499.455.830-26"}'
+      expect(result.values[2][3]).to eq '{"name" : "João Lima", "crm" : "B0000DHDOF", "crm_state" : "MA"}'
+    end
+
+    it 'retorna vazio caso não haja exames' do
+      repository = ExamsRepository.new conn: @conn
+
+      result = repository.all close_conn: false
+
+      expect(result.num_tuples).to eq 0
+      expect(result.values).to be_empty
+    end
+  end
 end
