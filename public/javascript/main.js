743dbfc98ceb15e601530a6a039f2c307d018613
Mateus Cavedini Pereira <mateuscavedini@gmail.com>
2024-03-14 04:13:44 -0300

    WIP: adiciona página web para busca/visualização de um exame

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,6 +1,10 @@
-const pageHeading = document.getElementById("page-heading")
+const pageHeading = document.getElementById("page-heading");
 const tableHeaderRow = document.getElementById("table-header-row");
 const tableBody = document.getElementById("table-body");
+const alertDiv = document.getElementById("alert");
+const backButton = document.getElementById("back-btn");
+const searchButton = document.getElementById("search-btn");
+const searchInput = document.getElementById("token-input");
 
 const setupTableHeaders = (headers) => {
   headers.forEach((header) => {
@@ -21,17 +25,19 @@ const setupTableBodyRow = (values) => {
     bodyRow.appendChild(bodyCell);
   });
 
-  tableBody.appendChild(bodyRow)
+  tableBody.appendChild(bodyRow);
 };
 
 const resetPage = () => {
-  tableHeaderRow.replaceChildren()
-  tableBody.replaceChildren()
-}
+  tableHeaderRow.replaceChildren();
+  tableBody.replaceChildren();
+  alertDiv.classList.add("not-displayed");
+  backButton.classList.add("not-displayed");
+};
 
 const handleExamsListing = () => {
-  resetPage()
-  pageHeading.textContent = "Exames Registrados"
+  resetPage();
+  pageHeading.textContent = "Exames Registrados";
 
   fetch("/exams")
     .then((response) => response.json())
@@ -63,4 +69,36 @@ const handleExamsListing = () => {
     .catch((err) => console.log(err));
 };
 
+const handleSearchFormSubmit = (e) => {
+  e.preventDefault();
+  const token = searchInput.value ? searchInput.value.toUpperCase() : null;
+
+  fetch(`/exams/${token}`)
+    .then((response) => response.json())
+    .then((data) => {
+      resetPage();
+      pageHeading.textContent = `Resultado por: ${token}`;
+
+      if (data.errors) {
+        alertDiv.textContent = data.errors;
+        alertDiv.classList.remove("not-displayed");
+        return;
+      }
+
+      const tableHeaders = ["Tipo", "Limites", "Resultado"];
+
+      setupTableHeaders(tableHeaders);
+
+      data.tests.forEach((test) => {
+        const rowValues = [test.type, test.limits, test.result];
+
+        setupTableBodyRow(rowValues);
+      });
+    })
+    .catch((err) => console.log(err))
+    .finally(() => backButton.classList.remove("not-displayed"));
+};
+
+searchButton.addEventListener("click", (e) => handleSearchFormSubmit(e));
+backButton.addEventListener("click", handleExamsListing);
 handleExamsListing();
